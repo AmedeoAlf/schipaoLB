1b9b8f589f251a117b3b965b4a108d62962db9e7
amedeo <amedeo.alfonsi@yahoo.com>
2026-01-14 18:44:59 +0100

    Correct onBlockBreak

--- src/main/kotlin/sh/ftp/schipao/schipaoLB/BlockDestroyListener.kt
+++ src/main/kotlin/sh/ftp/schipao/schipaoLB/BlockDestroyListener.kt
@@ -1,14 +1,14 @@
 package sh.ftp.schipao.schipaoLB
 
-import com.destroystokyo.paper.event.block.BlockDestroyEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 
 class BlockDestroyListener : Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
-    fun onBlockDestroy(event: BlockDestroyEvent?) {
+    fun onBlockBreak(event: BlockBreakEvent) {
         println("Block destroy event $event")
     }
 }
