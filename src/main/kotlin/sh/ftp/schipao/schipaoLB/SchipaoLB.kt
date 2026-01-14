d464b6f104a5aa85726c60be435d6ada21ba29f2
amedeo <amedeo.alfonsi@yahoo.com>
2026-01-14 18:06:20 +0100

    Initial commit

---
+++ src/main/kotlin/sh/ftp/schipao/schipaoLB/SchipaoLB.kt
@@ -0,0 +1,14 @@
+package sh.ftp.schipao.schipaoLB
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SchipaoLB : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
