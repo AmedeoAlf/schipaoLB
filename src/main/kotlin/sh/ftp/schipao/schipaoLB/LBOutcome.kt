7479b1922e9c2d9d4a9b460f03baf0ec877d8f91
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-01-16 22:16:47 +0100

    Organize some files

--- src/main/kotlin/sh/ftp/schipao/schipaoLB/LBOutcome.kt
+++ src/main/kotlin/sh/ftp/schipao/schipaoLB/LBOutcome.kt
@@ -1,8 +1,25 @@
 package sh.ftp.schipao.schipaoLB
 
 import io.papermc.paper.math.BlockPosition
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
 import org.bukkit.entity.Player
 
 interface LBOutcome {
     fun run(player: Player, position: BlockPosition)
+}
+
+val lbOutcomeModule = SerializersModule {
+    polymorphic(LBOutcome::class) {
+        subclass(TeleportOutcome::class)
+        subclass(ItemDropOutcome::class)
+        subclass(MessageOutcome::class)
+    }
+}
+
+val json = Json {
+    serializersModule = lbOutcomeModule
+    classDiscriminator = "type"
 }
