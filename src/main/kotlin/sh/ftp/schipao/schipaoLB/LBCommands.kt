8902f3e3c259aeecf374899f0faea62edfe39601
amedeo <amedeo.alfonsi@yahoo.com>
2026-01-17 18:35:40 +0100

    Initial commit of worldProtectorCmd

---
+++ src/main/kotlin/sh/ftp/schipao/schipaoLB/LBCommands.kt
@@ -0,0 +1,15 @@
+package sh.ftp.schipao.schipaoLB
+
+import com.mojang.brigadier.Command
+import io.papermc.paper.command.brigadier.Commands
+
+fun worldProtectorCmd(protector: WorldProtector, name: String = "worldprotector") =
+    Commands.literal(name)
+        .then(Commands.literal("save").executes {
+            protector.apply()
+            Command.SINGLE_SUCCESS
+        })
+        .then(Commands.literal("reset").executes {
+            protector.restore()
+            Command.SINGLE_SUCCESS
+        }).build()
--- src/main/kotlin/sh/ftp/schipao/schipaoLB/SchipaoLB.kt
+++ src/main/kotlin/sh/ftp/schipao/schipaoLB/SchipaoLB.kt
@@ -1,15 +1,22 @@
 package sh.ftp.schipao.schipaoLB
 
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.decodeFromStream
 import org.bukkit.Material
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 
+
 class SchipaoLB() : JavaPlugin() {
 
     override fun onEnable() {
         server.pluginManager.registerEvents(BlockDestroyListener(Material.DRIED_KELP_BLOCK, loadOutcomes()), this)
+        lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) {
+            it.registrar().register(
+                worldProtectorCmd(WorldProtector(server.respawnWorld))
+            )
+        }
     }
 
     override fun onDisable() {
