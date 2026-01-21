0b3e8ca4feab3c9d3039973a44a38229301079d8
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-01-21 20:05:00 +0100

    Allow MobOutcome.kt to specify multiple entities to spawn

--- src/main/kotlin/sh/ftp/schipao/schipaoLB/MobOutcome.kt
+++ src/main/kotlin/sh/ftp/schipao/schipaoLB/MobOutcome.kt
@@ -11,13 +11,25 @@ import org.bukkit.entity.Player
 @SerialName("mob")
 @Serializable
 class MobOutcome(val entities: List<String>) : LBOutcome {
+    // "PIG+PIG+PIG+PIG" => una stack di 4 PIG
+    // "PIG/4" => 4 PIG
     override fun run(player: Player, block: Block) {
-        entities
-            .map {
-                block.world.spawnEntity(block.location, EntityType.valueOf(it))
-                    .apply { teleport(this.location.add(0.0, height, 0.0)) }
+        entities.forEach {
+            if (it.contains("+")) {
+                it.split("+")
+                    .map {
+                        block.world.spawnEntity(block.location, EntityType.valueOf(it))
+                            .apply { teleport(this.location.add(0.0, height, 0.0)) }
+                    }
+                    .windowed(2)
+                    .map { (a, b) -> a.addPassenger(b) }
+            } else {
+                val (type, qty) = it.split("/") + "1"
+                (0..qty.toInt()).forEach { _ ->
+                    block.world.spawnEntity(block.location, EntityType.valueOf(type))
+                        .apply { teleport(this.location.add(0.0, height, 0.0)) }
+                }
             }
-            .windowed(2)
-            .forEach { (a, b) -> a.addPassenger(b) }
+        }
     }
 }
