c2ec1c3a3ac672ab5c446c0891abdd706e548582
PaoloSc <paolo95ita@gmail.com>
2026-01-29 22:04:52 +0100

    Player Interact with Compass

--- src/main/kotlin/sh/ftp/schipao/schipaoLB/SpawnListener.kt
+++ src/main/kotlin/sh/ftp/schipao/schipaoLB/SpawnListener.kt
@@ -2,6 +2,7 @@ package sh.ftp.schipao.schipaoLB
 
 import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -9,6 +10,7 @@ import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+
 
 class SpawnListener : Listener {
 
@@ -25,6 +27,16 @@ class SpawnListener : Listener {
         )
     }
 
+    @EventHandler
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        if (event.item != null && (event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK)) {
+            if (event.item!!.type == Material.COMPASS) {
+                for ((i, team) in GameManager.curr.teams.withIndex()) {
+                    GameManager.curr.teams.indexOfFirst { it.dyeColor == ilColoreDelBloccoInMano }
+                }
+            }
+        }
+    }
 
     @EventHandler
     fun onPlayerRespawn(event: PlayerPostRespawnEvent) {
